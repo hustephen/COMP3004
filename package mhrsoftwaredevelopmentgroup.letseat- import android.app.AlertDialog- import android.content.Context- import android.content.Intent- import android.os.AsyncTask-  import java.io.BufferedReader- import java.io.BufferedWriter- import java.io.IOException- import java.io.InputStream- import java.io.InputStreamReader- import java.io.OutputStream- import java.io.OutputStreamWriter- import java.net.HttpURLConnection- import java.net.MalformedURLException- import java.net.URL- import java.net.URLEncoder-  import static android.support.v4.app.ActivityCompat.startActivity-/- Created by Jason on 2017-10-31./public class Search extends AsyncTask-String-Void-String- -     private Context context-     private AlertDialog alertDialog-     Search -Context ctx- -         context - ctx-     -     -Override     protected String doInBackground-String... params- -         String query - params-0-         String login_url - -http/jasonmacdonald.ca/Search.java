38914b702b4ad79e252a9362d4552ded1ed6b603
JasonMacDonaldGithub <32023124+JasonMacDonaldGithub@users.noreply.github.com>
2017-11-18 19:10:54 -0500

    Create Search.java

---
+++ package mhrsoftwaredevelopmentgroup.letseat- import android.app.AlertDialog- import android.content.Context- import android.content.Intent- import android.os.AsyncTask-  import java.io.BufferedReader- import java.io.BufferedWriter- import java.io.IOException- import java.io.InputStream- import java.io.InputStreamReader- import java.io.OutputStream- import java.io.OutputStreamWriter- import java.net.HttpURLConnection- import java.net.MalformedURLException- import java.net.URL- import java.net.URLEncoder-  import static android.support.v4.app.ActivityCompat.startActivity-/- Created by Jason on 2017-10-31./public class Search extends AsyncTask-String-Void-String- -     private Context context-     private AlertDialog alertDialog-     Search -Context ctx- -         context - ctx-     -     -Override     protected String doInBackground-String... params- -         String query - params-0-         String login_url - -http/jasonmacdonald.ca/Search.java
@@ -0,0 +1,88 @@
+package mhrsoftwaredevelopmentgroup.letseat;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import static android.support.v4.app.ActivityCompat.startActivity;
+
+/**
+ * Created by Jason on 2017-10-31.
+ */
+public class Search extends AsyncTask<String,Void,String> {
+    private Context context;
+    private AlertDialog alertDialog;
+    Search (Context ctx) {
+        context = ctx;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        String query = params[0];
+        String login_url = "http://jasonmacdonald.ca/search.php";
+        if(query.equals("search")) {
+            try {
+                String price = params[1];
+                String rating = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("price","UTF-8")+"="+URLEncoder.encode(price,"UTF-8")+"&"
+                        +URLEncoder.encode("rating","UTF-8")+"="+URLEncoder.encode(rating, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
+
